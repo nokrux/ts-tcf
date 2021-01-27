4fbdfc2a885988c521e9849db76aa84609a0ff31
judland <julien.dland@gmail.com>
2021-01-27 16:20:15 +0100

    hello command in locator

--- src/services/locator/abstractlocator.ts
+++ src/services/locator/abstractlocator.ts
@@ -1,10 +1,60 @@
+import { EState, IChannel } from "../../channel/ichannel";
+import { IPeer } from "../../peer/ipeer";
 import { IService } from "../iservice";
+import { ILocatorInterface } from "./ilocatorlistener";
 
-export abstract class AbstractLocator implements IService{
-    private static serviceName: string = "Locator";
-    
+export abstract class AbstractLocator implements ILocatorInterface{
+    private _name: string = "Locator";
+    private _channel: IChannel;
+
+    constructor(channel: IChannel) {
+        this._channel = channel;
+    }
+
+    hello(): void {
+        switch (this._channel.getState()) {
+            case EState.CLOSED:
+                this._channel.opening();
+                break;
+            case EState.OPENNING:
+                this._channel.open();
+                break;
+            case EState.OPEN:
+                return;
+        }
+        this._channel.sendEvent(this, 'Hello', ["Breakpoints", "Tool"]);
+    }
+    peerAdded(peer: IPeer): void {
+        throw new Error("Method not implemented.");
+    }
+    peerChanged(peer: IPeer): void {
+        throw new Error("Method not implemented.");
+    }
+    peerRemoved(peerID: string): void {
+        throw new Error("Method not implemented.");
+    }
+    peerHeartBeat(peerID: string): void {
+        throw new Error("Method not implemented.");
+    }
+    event(name: string, data: any): void {
+        switch (name) {
+            case "Hello":
+                switch (this._channel.getState()) {
+                    case EState.CLOSED:
+                        this._channel.opening();
+                        break;
+                    case EState.OPENNING:
+                        this._channel.open();
+                        break;
+                    case EState.OPEN:
+                        return;
+                }
+                break;
+        }
+    }
+
     getName(): string {
-        return AbstractLocator.serviceName;
+        return this._name;
     }
 
 }
--- src/services/locator/ilocatorlistener.ts
+++ src/services/locator/ilocatorlistener.ts
@@ -1,5 +1,10 @@
+import { IEventListener } from "../../event/ieventlistener";
 import { IPeer } from "../../peer/ipeer";
 
-export interface ILocatorInterface{
+export interface ILocatorInterface extends IEventListener{
+    hello(): void;
     peerAdded(peer: IPeer): void;
+    peerChanged(peer: IPeer): void;
+    peerRemoved(peerID: string): void;
+    peerHeartBeat(peerID: string): void;
 }
